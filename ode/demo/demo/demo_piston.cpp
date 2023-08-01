51892efa969e6e7bb8427e565bbe439aaa99914d
c-shibayama <chifuyu.shibayama.r7@dc.tohoku.ac.jp>
2023-08-01 06:46:03 +0000

    make時のエラー修正

--- ode/demo/demo/demo_piston.cpp
+++ ode/demo/demo/demo_piston.cpp
@@ -659,7 +659,7 @@ int main (int argc, char **argv)
                 {
                     int j = i+1;
                     if ( j+1 > argc      ||  // Check if we have enough arguments
-                            argv[j] == '\0' ||  // We should have a path here
+                            argv[j] == NULL ||  // We should have a path here
                             argv[j][0] == '-' ) // We should have a path not a command line
                         Help (argv);
                     else
